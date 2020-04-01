f538222a0b99a8eebe63c73a7da9b797e5acb302
AmulyaX <amulya.b520@gmail.com>
2020-04-01 15:06:22 +0530

    phoenix: Create entry for media configs
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>
    Co-authored-by: PeterCxy <peter@typeblog.net>

--- device.mk
+++ device.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2020 Potato Open Sauce Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Default A/B configuration.
 ENABLE_AB ?= true
 
@@ -253,6 +269,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Media Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_skunk.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_skunk.xml \
+    $(LOCAL_PATH)/configs/media_profiles_skunk.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_skunk.xml
 
 # USB default HAL
 PRODUCT_PACKAGES += \
