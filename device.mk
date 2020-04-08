b13e79164d856b3cd6b76066d3c155cc88ce5583
AmulyaX <amulya.b520@gmail.com>
2020-04-08 13:45:28 +0530

    phoenix: Build DRM service and clearkey
    * sur fix L1

--- device.mk
+++ device.mk
@@ -136,7 +136,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
-    vendor.qti.hardware.display.mapper@1.1.vendor 
+    vendor.qti.hardware.display.mapper@1.1.vendor
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # F2FS utilities
  PRODUCT_PACKAGES += \
