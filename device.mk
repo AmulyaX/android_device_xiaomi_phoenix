c7a8af7060399468730786440589ece7baeef9e8
AmulyaX <amulya.b520@gmail.com>
2020-04-08 15:47:41 +0530

    phoenix: Import WiFi configs from stock

--- device.mk
+++ device.mk
@@ -382,6 +382,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 #FEATURE_OPENGLES_EXTENSION_PACK support string config file
 
 
